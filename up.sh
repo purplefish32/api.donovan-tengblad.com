8ace4769e6402e1156ca0c0c38f5659fb75debc0
Donovan Tengblad <purplefish32@gmail.com>
2017-10-14 17:28:41 +0200

    Group docker-compose files

--- up.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Creating overlay network"
-docker network create --driver=overlay --attachable proxy_network
-
-stacks/proxy/up.sh
-stacks/nats/up.sh
-stacks/ws-tcp-relay/up.sh
-stacks/api/up.sh
