be6d8a540090e5cb027f215322ee6c983be4a895
Donovan Tengblad <purplefish32@gmail.com>
2017-10-14 14:41:25 +0200

    WIP HTTPS

--- up.sh
+++ up.sh
@@ -3,8 +3,8 @@
 source .env
 echo "Creating overlay network"
 docker network create --driver=overlay --attachable proxy_network
-docker network create --driver=overlay --attachable nats_network
 
 stacks/proxy/up.sh
 stacks/nats/up.sh
 stacks/ws-tcp-relay/up.sh
+stacks/api/up.sh
