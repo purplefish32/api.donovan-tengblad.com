4e24bf5561eb6cf529f067c21839cd59373ef2a8
Donovan Tengblad <purplefish32@gmail.com>
2017-10-15 22:20:54 +0200

    Added clear cache command

--- up.sh
+++ up.sh
@@ -3,4 +3,5 @@ docker-compose up -d --build
 sleep 5
 docker-compose exec -T app bin/console security:check
 docker-compose exec -T app bin/console doctrine:schema:update --force
+docker-compose exec -T app bin/console cache:clear --env=prod
 # TODO migrations
