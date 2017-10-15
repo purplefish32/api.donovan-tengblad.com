67a0113c33025dc08be4a3f48a6c0fdf7fe2f7ce
Donovan Tengblad <purplefish32@gmail.com>
2017-10-15 21:41:41 +0200

    Added -T do docker-compose exec commands

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker-compose up -d --build
 sleep 5
-docker-compose exec app bin/console security:check
-docker-compose exec app bin/console doctrine:schema:update --force
+docker-compose exec -T app bin/console security:check
+docker-compose exec -T app bin/console doctrine:schema:update --force
 # TODO migrations
