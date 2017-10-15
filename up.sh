8c7ff88505114c36fbbd61698b3f8e1adf06671a
Donovan Tengblad <purplefish32@gmail.com>
2017-10-15 20:56:56 +0200

    Updated deploy script as mysql was not ready

--- up.sh
+++ up.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 docker-compose up -d --build
-docker-compose run --rm app bin/console security:check
-docker-compose run --rm app bin/console doctrine:schema:update --force
+sleep 5
+docker-compose exec app bin/console security:check
+docker-compose exec app bin/console doctrine:schema:update --force
 # TODO migrations
