6df56607102f7ca31af5f08dddb75aba5af31db1
Donovan Tengblad <purplefish32@gmail.com>
2017-11-03 08:36:45 +0100

    WIP

--- docker/api/app/AppKernel.php
+++ docker/api/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new AppBundle\AppBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
